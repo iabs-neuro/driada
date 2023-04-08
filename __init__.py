e861e2a1880fd6a0553cd0c5a93e54bbb8c89c27
niveousdragon <nik-pos@yandex.ru>
2023-04-08 19:45:14 +0300

    explicit imports in __init__

--- __init__.py
+++ __init__.py
@@ -1,6 +1,14 @@
+
+from .gdrive import download, upload
+from .information import gcmi, ksg, info_main
+from .signals import sig_main
+from .utils import data, naming, output, plot
+
+
+# TODO: probably add automatic scanning for every .py file (dangerous and not pythonic, but convenient)
+'''
 import glob
 from os.path import dirname, basename, isfile, join
-
-#modules = glob.glob(join(dirname(__file__), "*.py"))
 modules = glob.glob(join(dirname(__file__), "*", "*.py"), recursive=True)
 __all__ = [basename(f)[:-3] for f in modules if isfile(f) and not basename(f).startswith('_')]
+'''
